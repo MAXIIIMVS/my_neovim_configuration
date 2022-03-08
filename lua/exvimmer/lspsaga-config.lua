907167db4b29f9be305504319472ed0ad76ca477
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-08 19:17:50 +0330

    Add keybindings for lspsaga

--- lua/exvimmer/lspsaga-config.lua
+++ lua/exvimmer/lspsaga-config.lua
@@ -5,12 +5,12 @@ lspsaga.setup {
   -- diagnostic sign
   error_sign = "",
   warn_sign = "",
-  hint_sign = "",
+  -- hint_sign = "",
   infor_sign = "",
   diagnostic_header_icon = "   ",
   -- code action title icon
-  code_action_icon = " ",
-  code_action_prompt = {enable = true, sign = true, sign_priority = 40, virtual_text = true},
+  -- code_action_icon = " ",
+  code_action_prompt = {enable = false, sign = false, sign_priority = 40, virtual_text = true},
   finder_definition_icon = "  ",
   finder_reference_icon = "  ",
   max_preview_lines = 10,
