e37e1f77a6598fe51c7119416c74cfa6b479a51c
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-03-10 13:50:47 +0330

    Make html language server to support html, handlebars, htmldjango and blade

--- lua/exvimmer/lsp/language_servers.lua
+++ lua/exvimmer/lsp/language_servers.lua
@@ -2,7 +2,6 @@ local lsp_installer = require('nvim-lsp-installer')
 
 local function on_attach(client, bufnr)
   -- formatting
-  -- if client.name == 'tsserver' or client.name == 'gopls' then
   if client.name == 'tsserver' or client.name == 'html' then
     client.resolved_capabilities.document_formatting = false
     client.resolved_capabilities.document_range_formatting = false
@@ -21,17 +20,12 @@ lsp_installer.on_server_ready(function(server)
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   local opts = {capabilities = capabilities, on_attach = on_attach}
 
-  -- formatting
---   if client.name == 'tsserver' then
---     client.resolved_capabilities.document_formatting = false
---   end
 
---   if client.resolved_capabilities.document_formatting then
---     vim.api.nvim_command [[augroup Format]]
---     vim.api.nvim_command [[autocmd! * <buffer>]]
---     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
---     vim.api.nvim_command [[augroup END]]
---   end
+  if server.name == "html" then
+    opts = vim.tbl_deep_extend("force", {
+      filetypes = {"html","handlebars","htmldjango","blade"}
+    }, opts)
+  end
 
   if server.name == "sumneko_lua" then
     opts = vim.tbl_deep_extend("force", {
