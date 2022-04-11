b76d56d260520587e924a6817c454b373bb08a39
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-11 15:31:44 +0430

    Move keybindings for hop to the config file

--- lua/exvimmer/hop-config.lua
+++ lua/exvimmer/hop-config.lua
@@ -1,1 +1,9 @@
 require'hop'.setup()
+
+local u = require('utils')
+local cmd = vim.cmd
+u.map('n', '<space>;', ':HopWordMW<CR>', opts)
+u.map('n', '<space>/', ':HopPatternMW<CR>', opts)
+u.map('n', '<space>\'', ':HopLineMW<CR>', opts)
+u.map('n', '<space>f', ':HopChar1MW<CR>', opts)
+
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -145,12 +145,6 @@ u.map('n', '<space>m', ':MaximizerToggle<CR>', opts)
 -- choosewin
 -- u.map('n', '<space>w', ':ChooseWin<CR>', opts)
 
--- hop
-u.map('n', '<space>;', ':HopWordMW<CR>', opts)
-u.map('n', '<space>/', ':HopPatternMW<CR>', opts)
-u.map('n', '<space>\'', ':HopLineMW<CR>', opts)
-u.map('n', '<space>f', ':HopChar1MW<CR>', opts)
-
 -- vim-fugitive
 u.map('n', '<space>gs', ':G<CR>', opts)
 u.map('n', '<space>gS', ':G switch ', {noremap = true})
