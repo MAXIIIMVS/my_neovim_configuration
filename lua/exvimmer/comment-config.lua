8c2a5d7595099e016d5d5563a646a68488534153
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-16 18:44:17 +0430

    Change the keybinding for commenting code in nvim_comment plugin

--- lua/exvimmer/comment-config.lua
+++ lua/exvimmer/comment-config.lua
@@ -1,7 +1,20 @@
 require('nvim_comment').setup({
-  -- line_mapping = "<leader>cl",
-  -- operator_mapping = "<leader>c",
-  comment_empty = false
+  -- Linters prefer comment and line to have a space in between markers
+  marker_padding = true,
+  -- should comment out empty or whitespace only lines
+  comment_empty = false,
+  -- trim empty comment whitespace
+  comment_empty_trim_whitespace = true,
+  -- Should key mappings be created
+  create_mappings = true,
+  -- Normal mode mapping left hand side
+  line_mapping = "<c-_>", -- this is the replacement for <c-/>
+  -- Visual/Operator mapping left hand side
+  operator_mapping = "gc",
+  -- text object mapping, comment chunk,,
+  comment_chunk_text_object = "ic",
+  -- Hook function to call before commenting takes place
+  hook = nil
 })
 
 
