9afae2bdce3e324b21666f3336bbcffecd9e7b7f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-04-22 04:13:50 +0430

    Revert hop keybindings to the previous settings

--- lua/exvimmer/hop-config.lua
+++ lua/exvimmer/hop-config.lua
@@ -3,8 +3,7 @@ local opts = { noremap=true, silent=true }
 
 require'hop'.setup()
 
-u.map('n', ';w', ':HopWordMW<CR>', opts)
-u.map('n', ';p', ':HopPatternMW<CR>', opts)
-u.map('n', ';l', ':HopLineMW<CR>', opts)
-u.map('n', ';c', ':HopChar1MW<CR>', opts)
-
+u.map('n', '<space>;', ':HopWordMW<CR>', opts)
+u.map('n', '<space>/', ':HopPatternMW<CR>', opts)
+u.map('n', '<space>\'', ':HopLineMW<CR>', opts)
+u.map('n', '<space>,', ':HopChar1MW<CR>', opts)
