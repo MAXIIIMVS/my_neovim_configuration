88805692b6688a328012f4b3f9231888dab6f57e
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-04 10:19:30 +0430

    Add another keymap to format only the current file.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,9 +13,10 @@ u.map('n', '<leader>o', ':!xdg-open %<CR>', opts)
 u.map('n', '<leader>O', ':!xdg-open .<CR>', opts)
 
 -- run prettier in the current directory
-u.map('n', '<leader>p',
+u.map('n', '<leader>P',
   ':silent !cd ' .. u.get_top_level() .. '&& prettier --ignore-path .gitignore -w .<CR>:redraw!<CR>',
   opts)
+u.map('n', '<leader>p', ':silent !prettier -w %<CR>:redraw!<CR>')
 
 -- Resize window
 u.map('n', '<c-w><left>', '<c-w><')
