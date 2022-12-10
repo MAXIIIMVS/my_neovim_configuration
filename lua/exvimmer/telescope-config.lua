8c42667fdff8b356c773a035a3386361588b84c3
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-12-10 14:06:05 +0330

    Close the buffer from telescope.

--- lua/exvimmer/telescope-config.lua
+++ lua/exvimmer/telescope-config.lua
@@ -36,8 +36,14 @@ require('telescope').setup {
     buffer_previewer_maker = require 'telescope.previewers'.buffer_previewer_maker,
     mappings = {
       n = {
-        ["q"] = actions.close
-      },
-    }
+        ["q"] = actions.close,
+        ['<c-d>'] = require('telescope.actions').delete_buffer,
+        ["<C-h>"] = "which_key",
+      }, -- n
+      i = {
+        ["<C-h>"] = "which_key",
+        ['<c-d>'] = require('telescope.actions').delete_buffer
+      } -- i
+    } -- mappings
   },
 }
