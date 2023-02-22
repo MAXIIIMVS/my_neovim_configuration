bde6e17c2d96287c6836ea5cd24a2d5a49889d06
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-02-22 13:43:41 +0330

    Fix zen-mode issue completely.

--- after/plugin/zen-mode.lua
+++ after/plugin/zen-mode.lua
@@ -1,12 +1,14 @@
-local lualine = require("lualine")
+-- local lualine = require("lualine")
 
 require("zen-mode").setup({
 	---@diagnostic disable-next-line: unused-local
 	on_open = function(win)
-		lualine.hide({})
-		vim.o.statusline = "" -- "" is same as "%t %m"
+		-- lualine.hide({})
+		-- vim.o.statusline = "" -- "" is same as "%t %m"
+		vim.o.cmdheight = 1
 	end,
 	on_close = function()
-		lualine.hide({ unhide = true })
+		vim.o.cmdheight = 0
+		-- lualine.hide({ unhide = true })
 	end,
 })
